d2d625a10f0bb763d9ede3abf57667ea08cab38d
Marcio Bueno <marciobueno@gmail.com>
2026-02-20 08:57:07 -0300

    tags audio, video, img, h1, p, hr, br, a

--- next-01/app/page.js
+++ next-01/app/page.js
@@ -1,5 +1,157 @@
-import styles from "./page.module.css";
-
 export default function Home() {
-  return <div className={styles.page}>Olá, Turma!!!</div>;
+  return (
+    <div>
+      <a href="https://www.google.com.br" target="_blank">
+        Ir para o Google
+      </a>
+      <br />
+      <a href="#plantas">Ir para a seção plantas</a>
+      <br />
+      <img
+        src="https://plus.unsplash.com/premium_photo-1683910767532-3a25b821f7ae?q=80&w=808&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        width={320}
+      ></img>
+      <img src="lago.jpg" width={320} height={426}></img>
+      <hr />
+      <audio src="horse.mp3" controls autoPlay>
+        Seu navegador não suporta a tag audio.
+      </audio>
+      <video src="movie.mp4" controls>
+        Seu navegador não suporta a tag vídeo.
+      </video>
+      <hr />
+      <h1>Lista de plantas silvestres na Amazônia</h1>
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        Bromeliaceae Juss. é uma família de monocotiledôneas, que segundo a
+        classificação filogenética AGP III (2009) <br /> pertence à ordem dos
+        Poales, com plantas terrestres, rupícolas ou, principalmente epífitas,
+        possui 3.172 espécies, distribuídas em 58 gêneros.
+      </p>
+      <hr />
+      <p id="plantas">
+        Chrysobalanaceae é uma família de plantas angiospérmicas (plantas com
+        flor - divisão Magnoliophyta), pertencente à ordem Malpighiales.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+      <hr />
+      <p>
+        A lista de plantas silvestres da Amazônia inclui todas as espécies
+        endêmicas da vegetação da Amazônia. <br /> Segundo Giacometti (1990), é
+        estimada a existência de 800 espécies vegetais de valor econômico ou
+        social nesta floresta. Elas foram listadas abaixo de acordo com a
+        família à qual pertencem.
+      </p>
+    </div>
+  );
 }
